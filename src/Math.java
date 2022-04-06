5fcdece3b67ed140119f4b1a2203d950b48fc5d1
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-04-06 15:21:38 -0400

    Add files via upload

---
+++ src/Math.java
@@ -0,0 +1,14 @@
+
+public class Math {
+    public static void main(String[] args)
+    {
+        //Should output 15
+        System.out.println(f(5));
+    }
+    
+    
+    public static int f(int x){
+        
+        
+    }
+}
